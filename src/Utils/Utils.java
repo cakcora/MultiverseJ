aa2f263030d277309e291ea4ac6673f73adb3391
tdagraphs <61297344+tdagraphs@users.noreply.github.com>
2021-03-07 19:51:53 -0600

    saving graphs

--- src/Utils/Utils.java
+++ src/Utils/Utils.java
@@ -2,11 +2,14 @@ package Utils;
 
 import core.DataPoint;
 import core.Dataset;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.util.Pair;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Map;
 
 public class Utils {
     public static <T> double getAverage(Collection<T> myList) {
@@ -20,16 +23,29 @@ public class Utils {
 
     public static void save(String filePath, Dataset dataset) throws IOException {
         BufferedWriter wr = new BufferedWriter(new FileWriter(filePath));
-        String featureNames [] = dataset.getFeatureNames();
+        String[] featureNames = dataset.getFeatureNames();
         for(String s:featureNames){
             wr.write(s+"\t");
         }
         wr.write("label\r\n");
-        for(DataPoint dp:dataset.getDatapoints()){
-            for(double f:dp.getFeatures()){
-                wr.write(f+"\t");
+        for (DataPoint dp : dataset.getDatapoints()) {
+            for (double f : dp.getFeatures()) {
+                wr.write(f + "\t");
             }
-            wr.write(dp.getLabel()+"\r\n");
+            wr.write(dp.getLabel() + "\r\n");
+        }
+        wr.close();
+    }
+
+    public static void saveGraphs(String filePath, Map<Integer, Graph<Integer, Integer>> graphs) throws IOException {
+        BufferedWriter wr = new BufferedWriter(new FileWriter(filePath));
+        wr.write("poison\tfrom\tto");
+        for (Integer poison : graphs.keySet()) {
+            Graph<Integer, Integer> graph = graphs.get(poison);
+            for (int edge : graph.getEdges()) {
+                Pair<Integer> endpoints = graph.getEndpoints(edge);
+                wr.write(poison + "\t" + endpoints.getFirst() + "\t" + endpoints.getSecond() + "\r\n");
+            }
         }
         wr.close();
     }
--- src/experiments/PoisonedLabelExperiment.java
+++ src/experiments/PoisonedLabelExperiment.java
@@ -5,6 +5,7 @@ import core.Dataset;
 import core.DecisionTree;
 import core.RandomForest;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
+import edu.uci.ics.jung.graph.Graph;
 import graphcore.GraphExtractor;
 import graphcore.GraphMetrics;
 import loader.CSVLoader;
@@ -59,6 +60,7 @@ public class PoisonedLabelExperiment {
 		//poisoning starts
 		Random random = new Random(151);
 		Dataset secondLevelDataset = new Dataset();
+		Map<Integer, Graph<Integer, Integer>> sampleGraphs = new HashMap<>();
 
 		for (int poisonLevel = 0; poisonLevel <= 30; poisonLevel += 3) {
 			LabelFlippingPoisoner poisoner = new LabelFlippingPoisoner(random);
@@ -72,8 +74,13 @@ public class PoisonedLabelExperiment {
 			rf.train(posionedDataset);
 
 
-			for (DecisionTree dt : rf.getDecisionTrees()) {
+			List<DecisionTree> decisionTrees = rf.getDecisionTrees();
+			GraphExtractor extractor = new GraphExtractor(decisionTrees.get(0));
+			sampleGraphs.put(poisonLevel, extractor.getGraph());
+
+			for (DecisionTree dt : decisionTrees) {
 				// extract a graph from the tree
+
 				GraphMetrics metric = computeGraphMetrics(dt);
 
 				if (metric.getVertexCount() > 0) {
@@ -86,17 +93,19 @@ public class PoisonedLabelExperiment {
 			}
 		}
 		int featureSize = secondLevelDataset.getFeatureNames().length;
-		Map<Integer,Integer> featureMap = new HashMap<>();
-		for(int i=0;i<featureSize;i++){
-			featureMap.put(i,i);
+		Map<Integer, Integer> featureMap = new HashMap<>();
+		for (int i = 0; i < featureSize; i++) {
+			featureMap.put(i, i);
 		}
 		secondLevelDataset.setFeatureParents(featureMap);
-		Utils.Utils.save("c://adultmetrics.txt",secondLevelDataset);
-		Dataset [] split = secondLevelDataset.split(0.8,0.20);
-		Dataset training =split[0];
+		Utils.Utils.save("c://adultmetrics.txt", secondLevelDataset);
+		Utils.Utils.saveGraphs("c://adultGraphs.txt", sampleGraphs);
+
+		Dataset[] split = secondLevelDataset.split(0.8, 0.20);
+		Dataset training = split[0];
 
 		// variable importance detection - on 2nd level random forest
-		System.out.println("Second level random forest has "+training.getDatapoints().size()+" data points");
+		System.out.println("Second level random forest has " + training.getDatapoints().size() + " data points");
 
 		// in the second level we do not have any one hot encoding, so everyu feature is derived from itself only.
 
